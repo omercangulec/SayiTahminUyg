f9b0670aabd18383eb44fe129688faa2356f35b0
omercangulec <omercangulec@hotmail.com>
2023-01-15 15:10:24 +0300

    Initial Commit

---
+++ SayiTahminUyg/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SayiTahminUyg
+//
+//  Created by Ömer Can Güleç on 15.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
